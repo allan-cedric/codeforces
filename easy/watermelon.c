655a705613c2fd7c01cf2418c41ad79f9673ee48
Allan Cedric <allan.py3000@gmail.com>
2020-05-03 09:37:54 -0300

    new adjust watermelon.c

--- easy/watermelon.c
+++ easy/watermelon.c
@@ -4,6 +4,6 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    !(n % 2) && (n - 2) ? printf("YES\n") : printf("NO\n");
+    !(n % 2) && n > 2 ? printf("YES\n") : printf("NO\n");
     return 0;
 }
