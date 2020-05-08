50ddc1baea3d83bec7b37df8bb1898b551504d74
Allan Cedric <allan.py3000@gmail.com>
2020-05-07 22:59:38 -0300

    done hq9plus.cpp

---
+++ easy/hq9plus.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string p;
+    getline(cin, p);
+    for (int i = 0; i < p.length(); i++)
+    {
+        if (p[i] == 'H' || p[i] == 'Q' || p[i] == '9')
+        {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
+    return 0;
+}
