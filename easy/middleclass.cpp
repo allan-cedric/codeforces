297b5589195b31fbd09e49ebbf910bc9076b35d2
Allan Cedric <allan.py3000@gmail.com>
2020-06-03 09:41:02 -0300

    done middleclass.cpp

---
+++ easy/middleclass.cpp
@@ -0,0 +1,25 @@
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    int t, n, x;
+    uint64_t sum;
+    std::cin >> t;
+    while (t--)
+    {
+        std::cin >> n >> x;
+        sum = 0;
+        int savings[n];
+        for (int i = 0; i < n; sum += savings[i], i++)
+            std::cin >> savings[i];
+        std::sort(savings, savings + n);
+        for (int i = 0, size = n; i < size && (sum / n < x); i++, n--)
+            sum -= savings[i];
+        std::cout << n << std::endl;
+    }
+    return 0;
+}
