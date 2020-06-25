c3eb0daf48c49a0acbd5c4088d7d71e9b22b775f
allan-cedric <allan.py3000@gmail.com>
2020-06-25 16:43:37 -0300

    done fashionablee.cpp

---
+++ easy/fashionablee.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        cout << (n % 4 ? "NO" : "YES");
+        cout << "\n";
+    }
+    return 0;
+}
